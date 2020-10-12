727430cb39cc6e9b314263e0a36bd1420f4489fa
evanzelt <bplaxbro@gmail.com>
2020-10-12 16:19:54 -0400

    testing

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,19 @@
 ("#login-form").submit((e) => {
     e.preventDefault();
+
     let data = $("#login-form").serialize()
     console.log(data);
+
     setTimeout(() => {
-    $.ajax({
-        type: 'post',
-        url: 'http://api.bpnhs.org/login',
-        data: data,
-        success: function (res) {
-            if(res.status == "logged in") { 
-                location.href = "dashboard"
-            }   
-        }
-    });
-    }, 5000);
+        $.ajax({
+            type: 'post',
+            url: 'http://api.bpnhs.org/login',
+            data: data,
+            success: function (res) {
+                if(res.status == "logged in") { 
+                    location.href = "dashboard"
+                }   
+            }
+        });
+    }, 50000);
 })
