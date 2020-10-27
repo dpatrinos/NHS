7bd71ada61c5d21a45a13fff7bcae4201cbbcb87
evanzelt <bplaxbro@gmail.com>
2020-10-27 10:36:10 -0400

    login.js update

--- public/js/login.js
+++ public/js/login.js
@@ -8,16 +8,14 @@ $("#login-form").submit((e) => {
     let data = $("#login-form").serialize()
     console.log(data);
 
-    setTimeout(() => {
-        $.ajax({
-            type: 'post',
-            url: 'http://api.bpnhs.org/login',
-            data: data,
-            success: function (res) {
-                if(res.status == "logged in") { 
-                    location.href = "dashboard"
-                }   
+    $.ajax({
+        type: 'post',
+        url: 'http://api.bpnhs.org/login',
+        data: data,
+        success: function (res) {
+            if(res.status == "logged in") { 
+                location.href = "dashboard"
             }
-        });
-    }, 50000);
-})
+        }
+     });
+}
